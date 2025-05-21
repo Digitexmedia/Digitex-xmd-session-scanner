c9197e67169d64df1d5d76b3cd2e3355fbc5af2a
Digitexmedia <150277877+Digitexmedia@users.noreply.github.com>
2025-05-22 02:56:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const app = express();
 __path = process.cwd();
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 5000; // Changed to 5000 to match Dockerfile
+const PORT = process.env.PORT || 5000;
+console.log('Loading modules...');
 let server = require('./qr'),
     code = require('./pair');
+console.log('Modules loaded successfully');
 require('events').EventEmitter.defaultMaxListeners = 500;
 
 app.use('/server', server);
 app.use('/code', code);
 app.use('/pair', async (req, res, next) => {
   try {
+    console.log('Serving pair.html from:', __path + '/pair.html');
     res.sendFile(__path + '/pair.html');
   } catch (err) {
     console.error('Error serving pair.html:', err);
@@ -18,6 +21,7 @@ app.use('/pair', async (req, res, next) => {
 });
 app.use('/qr', async (req, res, next) => {
   try {
+    console.log('Serving qr.html from:', __path + '/qr.html');
     res.sendFile(__path + '/qr.html');
   } catch (err) {
     console.error('Error serving qr.html:', err);
@@ -26,6 +30,7 @@ app.use('/qr', async (req, res, next) => {
 });
 app.use('/', async (req, res, next) => {
   try {
+    console.log('Serving main.html from:', __path + '/main.html');
     res.sendFile(__path + '/main.html');
   } catch (err) {
     console.error('Error serving main.html:', err);
@@ -38,9 +43,3 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`
-Don't Forgot To Give Star.
-
- Server running on http://localhost:` + PORT);
-});
-
-module.exports = app;
